6784640beff9fe78b51c11f1ee2b78d688266818
Yasssh-bombale <yashbombale@gmail.com>
2025-06-25 13:04:11 +0530

    changes by yash

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Test = () => {
   return (
     <div>
-      <p className='text-black'>Hello Yash</p>
+      <p className="text-black">Hello Yash</p>
+      <p>Hello ajeet sir changes by yash</p>
     </div>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
