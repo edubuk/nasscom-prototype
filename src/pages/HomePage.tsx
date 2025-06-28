d8df2bc6118550f883312884076ab3cebf9e997a
Yasssh-bombale <yashbombale@gmail.com>
2025-06-28 13:11:05 +0530

    add water mark and icons for the topics

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,8 @@ import UserForm from "@/forms/UserForm";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 import nasscomImage from "@/assets/nasscom-logo.svg";
-
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { Button } from "@/components/ui/button";
 const HomePage = () => {
   const [userFormSubmited, setUserFormSubmited] = useState<boolean>(
     JSON.parse(localStorage.getItem("userFormSubmited") || "false")
@@ -14,23 +15,35 @@ const HomePage = () => {
     console.log("HEY SUBMITTED AND RESULTS STORED ON BLOCKCHAIN");
   };
   return (
-    <div className="w-full flex flex-col items-center pt-10">
+    <div className="w-full flex flex-col items-center pt-5">
       <div className="mx-auto flex flex-col md:flex-row items-center gap-5 mb-5">
         <img
           src={nasscomImage}
           alt="nasscom-logo"
           className="w-28 sm:w-32 lg:w-40 object-cover"
         />
-        <h1 className="text-2xl md:text-4xl lg:text-6xl tracking-tight mb-4">
+        <h1 className="text-2xl md:text-4xl lg:text-4xl tracking-tight text-center">
           Developer Confluence Bangalore
         </h1>
       </div>
       <div
         className={twMerge(
-          "border w-full rounded-xl shadow-lg p-6",
-          userFormSubmited ? "max-w-6xl" : "max-w-4xl"
+          "mt-5 md:mt-0 md:border-t w-full rounded-xl shadow-lg p-6 mb-10 ",
+          userFormSubmited ? "max-w-6xl" : "max-w-5xl"
         )}
       >
+        {/* Back button */}
+        {userFormSubmited && (
+          <Button
+            variant="outline"
+            className="flex items-center gap-2 cursor-pointer md:ml-5"
+            onClick={() => setUserFormSubmited(false)}
+          >
+            <MdKeyboardDoubleArrowLeft />
+            <span>Back</span>
+          </Button>
+        )}
+
         {/* form component */}
         {!userFormSubmited ? (
           <UserForm setIsFormSubmited={setUserFormSubmited} />
